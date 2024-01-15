04a414cd497348fc9b5b660984ff596028f9626e
dl-bk <BahtX_hn46@student.itstep.org>
2024-01-15 12:20:37 +0100

    ex5

--- lab1.sql
+++ lab1.sql
@@ -1,1 +1,2 @@
-SELECT DISTINCT group_name FROM Student_Grades
+SELECT DISTINCT min_subject_name FROM Student_Grades
+WHERE average_grade_per_year = (SELECT MIN(average_grade_per_year)FROM Student_Grades)
